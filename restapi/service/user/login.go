f4acb6335a3300b377784f64371aa9cd35fb6c36
kkxl <chj655700@163.com>
2023-04-24 01:13:35 +0800

    Bug-未创建的用户登录没有任何提示信息，已经删除的用户继续登录没有任何提示信息

--- restapi/service/user/login.go
+++ restapi/service/user/login.go
@@ -22,7 +22,7 @@ import (
 func SerLogin(reqLogin req.ReqLogin, c *gin.Context) param.Resp {
 	// 查询用户是否存在
 	userModel, err := user.DaoGetUserByUserName(reqLogin.UserName)
-	if err != nil {
+	if userModel == nil && err == nil {
 		failStruct := param.Resp{
 			Code: 10013,
 			Msg:  global.I18nMap["10013"],
